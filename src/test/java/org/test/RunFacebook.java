affd296cd3fafa742a837be2c65d23434e9ec062
Raja438 <n.chidambaram438@gmail.com>
2022-06-20 19:15:38 +0530

    Projects Structure2

---
+++ src/test/java/org/test/RunFacebook.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class RunFacebook {
+
+@Test
+private void testA1() {
+
+System.out.println("Selenium Start Today");	
+System.out.println("Java Start Yesterday");	
+System.out.println("Proect Class Start Tomorow");
+
+} 
+	
+}
